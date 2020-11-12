ecabb41a86c56fde6243eca35e6a6b6b6e81f618
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-11-12 18:33:41 +0900

    swiper

---
+++ swiper/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper ('.slider2', {
+    // パラメーター
+    loop: true,
+    freeMode: true,
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
