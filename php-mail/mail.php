7183957f995cb1da979d55657bd5bcf5305f6114
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-12-05 20:32:13 +0900

    test

--- php-mail/mail.php
+++ php-mail/mail.php
@@ -30,14 +30,16 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 //---------------------------　必須設定　必ず設定してください　-----------------------
 
 //サイトのトップページのURL　※デフォルトでは送信完了後に「トップページへ戻る」ボタンが表示されますので
-$site_top = "http://www.php-factory.net/";
+$site_top = "https://tomizawajun.com";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-$to = "xxxxxxxxxx@xxx.xxx";
+$to = "j.tomizawa.japan@gmail.com";
+//通知用メールアドレス
+$notice_to = "j.tomizawa.japan@gmail.com";
 
 //自動返信メールの送信元メールアドレス
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
-$from = "xxxxxxxxxx@xxx.xxx";
+$from = "j.tomizawa.japan@gmail.com";
 
 //フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
 $Email = "Email";
@@ -48,11 +50,11 @@ $Email = "Email";
 
 //スパム防止のためのリファラチェック（フォーム側とこのファイルが同一ドメインであるかどうかのチェック）(する=1, しない=0)
 //※有効にするにはこのファイルとフォームのページが同一ドメイン内にある必要があります
-$Referer_check = 0;
+$Referer_check = 1;
 
 //リファラチェックを「する」場合のドメイン ※設置するサイトのドメインを指定して下さい。
 //もしこの設定が間違っている場合は送信テストですぐに気付けます。
-$Referer_check_domain = "php-factory.net";
+$Referer_check_domain = "tomizawajun.com";
 
 /*セッションによるワンタイムトークン（CSRF対策、及びスパム防止）(する=1, しない=0)
 ※ただし、この機能を使う場合は↓の送信確認画面の表示が必須です。（デフォルトではON（1）になっています）
@@ -75,6 +77,8 @@ $BccMail = "";
 // 管理者宛に送信されるメールのタイトル（件名）
 $subject = "ホームページのお問い合わせ";
 
+$noticeSubject = "ホームページのお問い合わせがあります";
+
 // 送信確認画面の表示(する=1, しない=0)
 $confirmDsp = 1;
 
@@ -84,15 +88,15 @@ $confirmDsp = 1;
 $jumpPage = 0;
 
 // 送信完了後に表示するページURL（上記で1を設定した場合のみ）※httpから始まるURLで指定ください。（相対パスでも基本的には問題ないです）
-$thanksPage = "http://xxx.xxxxxxxxx/thanks.html";
+$thanksPage = "https://tomizawajun.com/contact/thanks";
 
 // 必須入力項目を設定する(する=1, しない=0)
-$requireCheck = 0;
+$requireCheck = 1;
 
 /* 必須入力項目(入力フォームで指定したname属性の値を指定してください。（上記で1を設定した場合のみ）
 値はシングルクォーテーションで囲み、複数の場合はカンマで区切ってください。フォーム側と順番を合わせると良いです。 
 配列の形「name="○○[]"」の場合には必ず後ろの[]を取ったものを指定して下さい。*/
-$require = array('お名前','Email');
+$require = array('お名前','Email','Email(確認用)','電話番号','お問い合わせ内容','個人情報の取り扱いについて');
 
 
 //----------------------------------------------------------------------
@@ -101,13 +105,13 @@ $require = array('お名前','Email');
 
 // 差出人に送信内容確認メール（自動返信メール）を送る(送る=1, 送らない=0)
 // 送る場合は、フォーム側のメール入力欄のname属性の値が上記「$Email」で指定した値と同じである必要があります
-$remail = 0;
+$remail = 1;
 
 //自動返信メールの送信者欄に表示される名前　※あなたの名前や会社名など（もし自動返信メールの送信者名が文字化けする場合ここは空にしてください）
 $refrom_name = "";
 
 // 差出人に送信確認メールを送る場合のメールのタイトル（上記で1を設定した場合のみ）
-$re_subject = "送信ありがとうございました";
+$re_subject = "お問い合わせありがとうございます。";
 
 //フォーム側の「名前」箇所のname属性の値　※自動返信メールの「○○様」の表示で使用します。
 //指定しない、または存在しない場合は、○○様と表示されないだけです。あえて無効にしてもOK
@@ -125,18 +129,13 @@ TEXT;
 
 
 //自動返信メールに署名（フッター）を表示(する=1, しない=0)※管理者宛にも表示されます。
-$mailFooterDsp = 0;
+$mailFooterDsp = 1;
 
 //上記で「1」を選択時に表示する署名（フッター）（FOOTER～FOOTER;の間に記述してください）
 $mailSignature = <<< FOOTER
 
 ──────────────────────
-株式会社○○○○　佐藤太郎
-〒150-XXXX 東京都○○区○○ 　○○ビル○F　
-TEL：03- XXXX - XXXX 　FAX：03- XXXX - XXXX
-携帯：090- XXXX - XXXX 　
-E-mail:xxxx@xxxx.com
-URL: http://www.php-factory.net/
+冨澤　潤
 ──────────────────────
 
 FOOTER;
@@ -156,7 +155,7 @@ $hankaku = 0;
 //全角英数字→半角変換を行う項目のname属性の値（name="○○"の「○○」部分）
 //※複数の場合にはカンマで区切って下さい。（上記で「1」を指定した場合のみ有効）
 //配列の形「name="○○[]"」の場合には必ず後ろの[]を取ったものを指定して下さい。
-$hankaku_array = array('電話番号','金額');
+$hankaku_array = array('電話番号');
 
 //-fオプションによるエンベロープFrom（Return-Path）の設定(する=1, しない=0)　
 //※宛先不明（間違いなどで存在しないアドレス）の場合に 管理者宛に「Mail Delivery System」から「Undelivered Mail Returned to Sender」というメールが届きます。
@@ -241,13 +240,19 @@ if(($confirmDsp == 0 || $sendmail == 1) && $empty_flag != 1){
 	$adminBody = mailToAdmin($_POST,$subject,$mailFooterDsp,$mailSignature,$encode,$confirmDsp);
 	$header = adminHeader($userMail,$post_mail,$BccMail,$to);
 	$subject = "=?iso-2022-jp?B?".base64_encode(mb_convert_encoding($subject,"JIS",$encode))."?=";
+	//通知先に届くメールをセット
+	$noticeBody = mailToNotice($_POST,$noticeSubject,$mailFooterDsp,$mailSignature,$encode,$confirmDsp);
+	$noticeHeader = noticeHeader($refrom_name,$from,$encode);
+	$noticeSubject = "=?iso-2022-jp?B?".base64_encode(mb_convert_encoding($noticeSubject,"JIS",$encode))."?=";
 	
 	//-fオプションによるエンベロープFrom（Return-Path）の設定(safe_modeがOFFの場合かつ上記設定がONの場合のみ実施)
 	if($use_envelope == 0){
 		mail($to,$subject,$adminBody,$header);
+		mail($notice_to,$noticeSubject,$noticeBody,$noticeHeader);
 		if($remail == 1 && !empty($post_mail)) mail($post_mail,$re_subject,$userBody,$reheader);
 	}else{
 		mail($to,$subject,$adminBody,$header,'-f'.$from);
+		mail($notice_to,$noticeSubject,$noticeBody,$noticeHeader,'-f'.$from);
 		if($remail == 1 && !empty($post_mail)) mail($post_mail,$re_subject,$userBody,$reheader,'-f'.$from);
 	}
 }
@@ -542,6 +547,20 @@ function adminHeader($userMail,$post_mail,$BccMail,$to){
 		$header.="Content-Type:text/plain;charset=iso-2022-jp\nX-Mailer: PHP/".phpversion();
 		return $header;
 }
+function noticeHeader($refrom_name,$to,$encode){
+	$reheader = "From: ";
+	if(!empty($refrom_name)){
+		$default_internal_encode = mb_internal_encoding();
+		if($default_internal_encode != $encode){
+			mb_internal_encoding($encode);
+		}
+		$reheader .= mb_encode_mimeheader($refrom_name)." <".$to.">\nReply-To: ".$to;
+	}else{
+		$reheader .= "$to\nReply-To: ".$to;
+	}
+	$reheader .= "\nContent-Type: text/plain;charset=iso-2022-jp\nX-Mailer: PHP/".phpversion();
+	return $reheader;
+}
 //管理者宛送信メールボディ
 function mailToAdmin($arr,$subject,$mailFooterDsp,$mailSignature,$encode,$confirmDsp){
 	$adminBody="「".$subject."」からメールが届きました\n\n";
@@ -558,6 +577,23 @@ function mailToAdmin($arr,$subject,$mailFooterDsp,$mailSignature,$encode,$confir
 	}
 	if($mailFooterDsp == 1) $adminBody.= $mailSignature;
 	return mb_convert_encoding($adminBody,"JIS",$encode);
+}
+function mailToNotice($arr,$subject,$mailFooterDsp,$mailSignature,$encode,$confirmDsp){
+	$noticeBody="ホームページのお問い合わせがあります。\n";
+	$noticeBody.="専用のメールアドレスを確認してください。\n";
+	$noticeBody.="\n";
+	/*
+	$noticeBody.="送信された日時：".date( "Y/m/d (D) H:i:s", time() )."\n";
+	$noticeBody.="送信者のIPアドレス：".@$_SERVER["REMOTE_ADDR"]."\n";
+	$noticeBody.="送信者のホスト名：".getHostByAddr(getenv('REMOTE_ADDR'))."\n";
+	*/
+	if($confirmDsp != 1){
+		$noticeBody.="問い合わせのページURL：".@$_SERVER['HTTP_REFERER']."\n";
+	}else{
+		$noticeBody.="問い合わせのページURL：".@$arr['httpReferer']."\n";
+	}
+	if($mailFooterDsp == 1) $noticeBody.= $mailSignature;
+	return mb_convert_encoding($noticeBody,"JIS",$encode);
 }
 
 //ユーザ宛送信メールヘッダ
