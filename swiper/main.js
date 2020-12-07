3f2bdf23bdec3eb4f08cb1decbf295be442602a8
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-12-07 19:24:14 +0900

    実行制御

--- swiper/main.js
+++ swiper/main.js
@@ -1,10 +1,28 @@
-var mySwiper = new Swiper ('.slider2', {
-    // パラメーター
+$(document).on('click', '.show', function(){
+  $('.slider2').fadeIn();
+
+  var mySwiper2 = new Swiper ('.slider2', {
+    initialSlide: 0,
     loop: true,
-    // freeMode: true,
-    
+    shortSwipes: false,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
   });
+});
+
+let slider2 = $('.slider2').html();
+
+$(document).on('click', '.hide', function(){
+  $.when(
+    // 先に実行したい処理
+    $('.slider2').fadeOut(),
+  ).done(function() {
+      // 後に実行したい処理
+    $('.slider2').removeAttr('style');
+    $('.slider2').attr('class', 'swiper-container slider2');
+    $('.slider2').html(slider2);
+	});
+});
+
